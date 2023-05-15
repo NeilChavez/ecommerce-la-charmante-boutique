eec72eb42163eb9efa1de23358c6929ba708715d
NeilChavez <lolochavez22@icloud.com>
2023-05-15 05:39:10 -0500

    refactor: change type

--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -1,13 +1,13 @@
-import { type Products } from '../types'
+import { type Product } from '../types'
 
-export const getProducts = async ({ endpoint }: { endpoint: string }): Promise<Products[]> => {
+export const getProducts = async ({ endpoint }: { endpoint: string }): Promise<Product[]> => {
   try {
     const res = await fetch(endpoint)
     if (!res.ok) {
       throw new Error('No se puedo obtener los productos!')
     }
-    const json: Products[] = await res.json()
-    const mappedProducts: Products[] = json.map(product => {
+    const json: Product[] = await res.json()
+    const mappedProducts: Product[] = json.map(product => {
       const { id, title, image, price, description, category, rating } = product
       return { id, title, image, price, description, category, rating }
     })
