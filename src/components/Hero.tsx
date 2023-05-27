a2489e70dec0ed6046ca5cd917483ba5632ce16b
NeilChavez <lolochavez22@icloud.com>
2023-05-27 09:06:23 -0500

    feat: change title in Hero Component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,8 @@ export const Hero: React.FC = () => {
   return (
     <section className="bg-[#F5E6E0]">
       <div className="m-auto max-w-4xl flex justify-around max-h-full">
-        <div className="flex-grow flex justify-center items-center">
-          <h2>Sono un titolo</h2>
+        <div className="flex-grow flex justify-center items-center pl-6 md:px-0 text-xl font-bold ">
+          <h2>Explore, Inspire, and Elevate Your Style</h2>
         </div>
         <img
           src="./public/assets/woman_hero.png"
